0164ca8892ff37f882cc3ae90f32d1698196e9ca
lucaliberti <liberti.luca@gmail.com>
2022-02-13 20:41:43 +0100

    finito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GeneraSalva01Component } from './genera-salva01/genera-salva01.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneraSalva01Component,
   ],
   imports: [
     BrowserModule
